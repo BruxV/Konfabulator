08454b30cb32f87bf07c209882ab08681d0baf13
Brux Voight <bruxvoight@sbcglobal.net>
2012-02-21 14:38:48 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konfabulator::Application.config.secret_token = 'e16cbceb6e6ef291a155323acefc91cf9e0e5bb7d9c79d02e66b7e24dd0883a20a9ab798946de7738b9a53eca8ae66c22ef01615bd8b4687ad4867380763915f'
